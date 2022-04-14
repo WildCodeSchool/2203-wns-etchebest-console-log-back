ba02ea4462fe3b87ceb05e0807533c597c3a1eee
Quentin Rault <q.rault054@gmail.com>
2022-04-14 16:11:55 +0200

    Convert to TypeGraph

---
+++ src/entities/Ticket.ts
@@ -0,0 +1,35 @@
+import { ObjectType, Field } from 'type-graphql';
+
+// const { gql } = require('apollo-server');
+
+@ObjectType()
+class Ticket {
+  @Field()
+  title: string;
+  
+  @Field()
+  description: string;
+}
+export default Ticket;
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+// const typeDefs = gql`
+//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+//   type Ticket {
+//     title: String
+//     description: String
+//   }
+
+//   type Query {
+//     getAllTickets: [Ticket]
+//   }
+
+//   type Mutation {
+//     createTicket(title: String, description: String): Ticket
+//   }
+// `;
+
+// export default typeDefs;
--- src/resolvers/resolvers.ts
+++ src/resolvers/resolvers.ts
@@ -1,33 +1,48 @@
+import { Resolver, Query } from 'type-graphql';
 import TicketModel from '../models/Ticket';
+import Ticket from '../entities/Ticket';
 
-// const tickets = [
-//   {
-//     title: 'The Awakening',
-//     description: 'Kate Chopin',
+
+@Resolver()
+class TicketResolver {
+
+  @Query(() => [Ticket])
+  async getAllTickets() {
+    const result = await TicketModel.find();
+    return result;
+  }
+}
+
+export default TicketResolver;
+
+// // const tickets = [
+//   // {
+//   //   title: 'The Awakening',
+//   //   description: 'Kate Chopin',
+//   // },
+// //   {
+// //     title: 'City of Glass',
+// //     description: 'Paul Auster',
+// //   },
+// // ];
+
+// const resolvers = {
+//   Query: {
+//     getAllTickets: async () => {
+//       const result = await TicketModel.find();
+//       return result;
+//     },
 //   },
-//   {
-//     title: 'City of Glass',
-//     description: 'Paul Auster',
+//   Mutation: {
+//     createTicket: async (_: any, args: any) => {
+//       const ticket = new TicketModel({
+//         title: args.title,
+//         description: args.description,
+//       });
+//       const result = await ticket.save();
+//       return result;
+//     },
 //   },
-// ];
+// };
 
-const resolvers = {
-  Query: {
-    getAllTickets: async () => {
-      const result = await TicketModel.find();
-      return result;
-    },
-  },
-  Mutation: {
-    createTicket: async (_: any, args: any) => {
-      const ticket = new TicketModel({
-        title: args.title,
-        description: args.description,
-      });
-      const result = await ticket.save();
-      return result;
-    },
-  },
-};
-
-export default resolvers;
+// export default resolvers;
