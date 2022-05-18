e30653045090a0735b3ca8af5a088bd75939c898
rima-chan <marie.beaujeu@gmail.com>
2022-05-18 16:10:58 +0200

    Add Project entitie

---
+++ src/entities/Project.ts
@@ -0,0 +1,40 @@
+import { ObjectType, Field, InputType, ID } from 'type-graphql';
+/* eslint max-classes-per-file: ["error", 2] */
+
+@ObjectType()
+class Project {
+  @Field(() => ID)
+  readonly _id: string | undefined;
+
+  @Field()
+  name: string;
+
+  @Field()
+  creationDate: Date;
+
+  @Field()
+  limitDate: Date;
+
+  @Field()
+  status: string;
+
+  @Field()
+  manager: string;
+}
+
+@InputType({ description: 'New project data' })
+class NewProjectInput implements Partial<Project> {
+  @Field()
+  name?: string;
+
+  @Field()
+  limitDate?: Date;
+
+  @Field()
+  status?: string;
+
+  @Field()
+  manager?: string;
+}
+
+export { Project, NewProjectInput };
